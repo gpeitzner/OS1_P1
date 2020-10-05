67cfca37377f8dd280409d9588bd6979fe7e5341
Guillermo Peitzner <guillermopeitzner@gmail.com>
2020-10-05 13:51:52 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# SO1_Proyecto1
+
+## Client
+    1. Insert path.
+    2. Insert address.
+    3. Show data.
+    4. Send data.
+
+## Server A and B
+    1. Kernel module (show ram and cpu usage).
+    2. NoSQL Database (mongo and docker).
+    3. Python API Rest (docker).
+    4. Docker Compose.
+
+## Server 1
+    1. API Rest (python docker).
+
+## Server 2
+    1. Web Server (nginx, react and docker).
---
+++ kernel_modules/so_info.c
@@ -0,0 +1,50 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/seq_file.h>
+#include <linux/mm.h>
+#include <linux/proc_fs.h>
+
+static int my_proc_show(struct seq_file *m, void *v){
+    struct sysinfo i;
+	int ram_usage; 
+    si_meminfo(&i);
+	ram_usage = 100 - ((i.totalram - i.freeram)* 100)/(i.totalram) ;
+    seq_printf(m, "{ram:%d}", ram_usage);
+    return 0;
+}
+
+static ssize_t my_proc_write(struct file* file, const char __user *buffer, size_t count, loff_t *f_pos){
+    return 0;
+}
+
+static int my_proc_open(struct inode *inode, struct file *file){
+	return single_open(file, my_proc_show, NULL);
+}
+
+static struct file_operations my_fops={
+	.owner = THIS_MODULE,
+	.open = my_proc_open,
+	.release = single_release,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.write = my_proc_write
+};
+
+static int __init so_info_init(void){
+	struct proc_dir_entry *entry;
+	entry = proc_create("so_info", 0777, NULL, &my_fops);
+	if(!entry) {
+		return -1;
+	}
+	return 0;
+}
+
+static void __exit so_info_exit(void){
+	remove_proc_entry("so_info", NULL);
+}
+
+module_init(so_info_init);
+module_exit(so_info_exit);
+MODULE_LICENSE("GPL");
