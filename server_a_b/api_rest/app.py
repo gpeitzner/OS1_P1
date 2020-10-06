e03a32087a666b91f0ef54542e134092cf406425
Guillermo Peitzner <guillermopeitzner@gmail.com>
2020-10-05 23:28:45 -0600

    [add] api finished

--- server_a_b/api_rest/app.py
+++ server_a_b/api_rest/app.py
@@ -1,18 +1,55 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from pymongo import MongoClient
+from datetime import datetime
 
 app = Flask(__name__)
 client = MongoClient('db', 27017)
 db = client.tododb
 
 
-@app.route('/usage')
-def usage():
+@app.route("/usage")
+def get_usage():
     f = open("/procs/so_info", "r")
     response = str(f.read()).split(";")
     f.close()
     return jsonify({
         "ram": response[0],
-        "cpu": response[1]
+        "cpu": response[1],
+        "database": db.tododb.count()
     })
+
+
+@app.route("/sentence", methods=["POST"])
+def add_sentence():
+    sentence = {
+        "author": request.json["author"],
+        "sentence": request.json["sentence"],
+        "date": str(datetime.now())
+    }
+    sentence_id = db.tododb.insert_one(sentence)
+    new_sentence = db.tododb.find_one({"_id": sentence_id.inserted_id})
+    result = {
+        "author": new_sentence["author"],
+        "sentence": new_sentence["sentence"],
+        "date": new_sentence["date"]
+    }
+    return jsonify(result)
+
+
+@app.route("/sentence", methods=["GET"])
+def get_sentences():
+    sentences = []
+    for sentence in db.tododb.find():
+        sentences.append({
+            "author": sentence["author"],
+            "sentence": sentence["sentence"],
+            "date": sentence["date"]
+        })
+    return jsonify(sentences)
+
+
+@app.route("/restore", methods=["GET"])
+def restore():
+    eliminated = db.tododb.delete_many({})
+    return jsonify({"deleted": eliminated.deleted_count})
