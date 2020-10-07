bdd6934aef2095726f3e6f9cb8bcd9f8966a48a5
Guillermo Peitzner <guillermopeitzner@gmail.com>
2020-10-06 21:05:07 -0600

    [add] web server

---
+++ server_2/web/build/precache-manifest.b74839bd78ce7d0416902efcdd202690.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "588f2fe4a38bc2d42581d6970399d683",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cc9f6b832b604cca613",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "663dc1e629b0432121cc",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0cc9f6b832b604cca613",
+    "url": "/static/js/2.875d4928.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.875d4928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "663dc1e629b0432121cc",
+    "url": "/static/js/main.278c13d3.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  }
+]);
